2c1d0b06ffcc0c4f169e081ede49217fb6f2fd21
Peter Nam <100635863+pnam0228@users.noreply.github.com>
2022-08-03 09:06:39 -0400

    Update Frequency.scala

--- parallel-letter-frequency/src/main/scala/Frequency.scala
+++ parallel-letter-frequency/src/main/scala/Frequency.scala
@@ -5,7 +5,7 @@ import scala.concurrent.{Await, Future}
 object Frequency {
   def frequency(numWorkers: Int, texts: Seq[String]): Map[Char, Int] = {
     val chars = texts.flatten.filter(_.isLetter).map(_.toLower)
-    val workChunks = chars.grouped(1 max chars.length / numWorkers).toList
+    val workChunks = chars.grouped(1 max (chars.length / numWorkers)).toList
     val charCountListsF = workChunks.map(charMapAsync)
     val charCountMapF = Future.foldLeft(charCountListsF)(Map.empty[Char, Int]){ case (charCountMap, charIntList) =>
       charIntList.foldLeft(charCountMap){ case (charIntMap, (c, count)) =>
